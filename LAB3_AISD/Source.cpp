4bbcc60d077f59d94f91dd2dd6ab1b54b98d725e
maeosha <mochkauha123@gmail.com>
2023-12-17 01:38:24 +0400

    Шейкерная сортировка

--- LAB3_AISD/Source.cpp
+++ LAB3_AISD/Source.cpp
@@ -32,3 +32,40 @@ stats sort_by_inserts(std::vector<int>::iterator start, std::vector<int>::iterat
 	return stats;
 }
 
+stats shaker_sort(std::vector<int>::iterator& start, std::vector<int>::iterator& end) {
+	stats stats;
+	bool swaped = 0;
+	stats.comparison_count++;
+	do {
+		swaped = 1;
+		auto index = start;
+		while (index < end - 1) {
+			if (*index > *(index + 1)) {
+				auto tmp_index = *(index + 1);
+				*(index + 1) = *index;
+				*index = tmp_index;
+				swaped = 0;
+				stats.copy_count++;
+				stats.comparison_count += 4;
+			}
+			index++;
+		}
+		end--;
+		index = end - 1;
+		while (index > start) {
+			if (*index < *(index - 1)) {
+				auto tmp_index = *(index - 1);
+				*(index - 1) = *index;
+				*index = tmp_index;
+				swaped = 0;
+				stats.comparison_count += 4;
+				stats.copy_count++;
+			}
+
+			index--;
+		}
+		start++;
+	} while (swaped == 0);
+
+	return stats;
+}
