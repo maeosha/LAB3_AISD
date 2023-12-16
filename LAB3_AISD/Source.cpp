c935c7698be2ce7e95a30f1aa865c4ca580b2d80
maeosha <mochkauha123@gmail.com>
2023-12-17 02:55:55 +0400

    создание функции для подсчета средних значений и запись их в файл

--- LAB3_AISD/Source.cpp
+++ LAB3_AISD/Source.cpp
@@ -1,6 +1,14 @@
 #include <vector>
 #include <fstream>
+#include <iostream>
 #include <random>
+
+
+enum SortMethod {
+	enum_sort_by_inserts = 1,
+	enum_shaker_sort = 2,
+	enum_comb_sort = 3
+};
 
 
 struct stats {
@@ -8,8 +16,10 @@ struct stats {
 	size_t copy_count = 0;
 };
 
+
 stats sort_by_inserts(std::vector<int>::iterator start, std::vector<int>::iterator end) {
 	stats stats;
+
 	for (auto border = start + 1; border < end; border++) {
 		const int key = *border;
 		auto border_elem = border - 1;
@@ -32,7 +42,7 @@ stats sort_by_inserts(std::vector<int>::iterator start, std::vector<int>::iterat
 	return stats;
 }
 
-stats shaker_sort(std::vector<int>::iterator& start, std::vector<int>::iterator& end) {
+stats shaker_sort(std::vector<int>::iterator start, std::vector<int>::iterator end) {
 	stats stats;
 	bool swaped = 0;
 	stats.comparison_count++;
@@ -71,14 +81,14 @@ stats shaker_sort(std::vector<int>::iterator& start, std::vector<int>::iterator&
 }
 
 
-stats comb_sort(std::vector<int>::iterator& start, std::vector<int>::iterator& end, const size_t size) {
+stats comb_sort(std::vector<int>::iterator start, std::vector<int>::iterator end, const size_t size) {
 	stats stats;
 	size_t step = size;
 	stats.comparison_count++;
 	while (step > 1) {
 		step = step / 1.247f;
 		auto  index = start;
-		auto end_elem = end - step;
+		auto end_elem = start + step - 1;
 		for (; index < end - step + 1; index++) {
 			auto elem = *(index + step - 1);
 			stats.comparison_count++;
@@ -92,4 +102,65 @@ stats comb_sort(std::vector<int>::iterator& start, std::vector<int>::iterator& e
 		}
 	}
 	return stats;
+}
+
+
+void average_stats(const size_t size, const int sort_method) {
+	size_t average_comparison_count;
+	size_t average_copy_count;
+	stats sorted_stats;
+	std::ofstream fout("stats.txt", std::ios_base::app);
+
+	fout << "(Array size, average comparison count) (Array size, average copy count)" << std::endl;
+
+	
+	for (size_t count = 0; count < 50; count++) {
+		stats tmp_stats;
+		std::vector<int> sort_array;
+
+		for (size_t index = 0; index < size; index++) {
+			std::random_device rd;
+			std::mt19937 gen(rd());
+			std::uniform_int_distribution<> dist(-1000, 1000);
+			sort_array.push_back(dist(gen));
+		}
+
+		auto start = sort_array.begin();
+		auto end = sort_array.end();
+
+
+		switch (sort_method) {
+		case enum_sort_by_inserts:
+			tmp_stats = sort_by_inserts(start, end);
+			break;
+		case enum_shaker_sort:
+			tmp_stats = shaker_sort(start, end);
+			break;
+
+		case enum_comb_sort:
+			tmp_stats = comb_sort(start, end, sort_array.size());
+			break;
+		}
+		sort_array.clear();
+
+		sorted_stats.comparison_count += tmp_stats.comparison_count;
+		sorted_stats.copy_count += tmp_stats.copy_count;
+	}
+	
+	average_comparison_count = sorted_stats.comparison_count / 10;
+	average_copy_count = sorted_stats.copy_count / 10;
+
+	fout << "(" << size << ", " << average_comparison_count << ") ";
+	fout << "(" << size << ", " << average_copy_count << ")" << sort_method << std::endl;
+
+	fout.close();
+
+	std::cout << "completed!";
+
+}
+
+int main() {
+	average_stats(1000, 1);
+	average_stats(1000, 2);
+	average_stats(1000, 3);
 }
