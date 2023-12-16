0387508fa9bc2b031f00b2ea0a4d83318188400f
maeosha <mochkauha123@gmail.com>
2023-12-17 01:36:28 +0400

    Создание сортировки вставками с аргументами в виде начального и конечного итератора

--- LAB3_AISD/Source.cpp
+++ LAB3_AISD/Source.cpp
@@ -1,2 +1,34 @@
+#include <vector>
+#include <fstream>
+#include <random>
 
+
+struct stats {
+	size_t comparison_count = 0;
+	size_t copy_count = 0;
+};
+
+stats sort_by_inserts(std::vector<int>::iterator start, std::vector<int>::iterator end) {
+	stats stats;
+	for (auto border = start + 1; border < end; border++) {
+		const int key = *border;
+		auto border_elem = border - 1;
+		while (border_elem >= start) {
+			if (*border_elem <= key) {
+				break;
+			}
+			else {
+				*(border_elem + 1) = *border_elem;
+				*border_elem = key;
+				stats.comparison_count += 2;
+			}
+			if (border_elem != start) {
+				border_elem--;
+			}
+			stats.copy_count++;
+		}
+	}
+
+	return stats;
+}
 
